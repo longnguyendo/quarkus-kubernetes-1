cd818a055a58f968741f8956808e3fb594311ef5
Logan Nguyen <longnguyen.open@gmail.com>
2025-11-06 22:54:17 +0700

    devtask1: 3 - Interface Repository

---
+++ src/main/java/org/lab/dev/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package org.lab.dev.repository;
+
+import org.lab.dev.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    List<Order> findByCartCustomerId(Long customerId);
+
+    Optional<Order> findByPaymentId(Long id);
+}
---
+++ src/main/java/org/lab/dev/repository/PaymentRepository.java
@@ -0,0 +1,14 @@
+package org.lab.dev.repository;
+
+import org.lab.dev.domain.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+    List<Payment> findAllByAmountBetween(BigDecimal min, BigDecimal max);
+}
