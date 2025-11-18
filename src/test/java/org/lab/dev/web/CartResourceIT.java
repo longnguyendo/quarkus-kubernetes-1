b77dd83da4dabada4f6e985d4048a9b4bdcd4d1d
Logan Nguyen <longnguyen.open@gmail.com>
2025-11-18 21:25:08 +0700

    devTask5: no testing:)

--- src/test/java/org/lab/dev/web/CartResourceIT.java
+++
@@ -1,8 +0,0 @@
-package org.lab.dev.web;
-
-//import io.quarkus.test.junit.NativeImageTest;
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class CartResourceIT extends CartResourceTest {
-}
